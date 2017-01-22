d06c76bf4b430fcde77483b11fb580dde93cd72b
Derek Shoemaker <shoemakerdr@gmail.com>
2017-01-22 02:03:02 +0000

    added instructions for 3rd exercise for chapter 4

--- elqjs4.js
+++ elqjs4.js
@@ -109,3 +109,40 @@ Thinking on the question of which function is more pure and which would be more
 useful: I'm honestly not totally sure. I still need to get a better handle on
 concepts like "side effects" and "pure functions."
 */
+
+/*
+Exercise 3: A list
+
+Objects, as generic blobs of values, can be used to build all sorts of data 
+structures. A common data structure is the list (not to be confused with the 
+array). A list is a nested set of objects, with the first object holding a 
+reference to the second, the second to the third, and so on.
+
+var list = {
+  value: 1,
+  rest: {
+    value: 2,
+    rest: {
+      value: 3,
+      rest: null
+    }
+  }
+};
+
+A nice thing about lists is that they can share parts of their structure. For 
+example, if I create two new values {value: 0, rest: list} and {value: -1, rest: 
+list} (with list referring to the variable defined earlier), they are both 
+independent lists, but they share the structure that makes up their last three 
+elements. In addition, the original list is also still a valid three-element 
+list.
+
+Write a function arrayToList that builds up a data structure like the previous 
+one when given [1, 2, 3] as argument, and write a listToArray function that 
+produces an array from a list. Also write the helper functions prepend, which 
+takes an element and a list and creates a new list that adds the element to the 
+front of the input list, and nth, which takes a list and a number and returns 
+the element at the given position in the list, or undefined when there is no 
+such element.
+
+If you haven’t already, also write a recursive version of nth.
+*/
