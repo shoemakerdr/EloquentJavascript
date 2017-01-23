0c3a490ed548e12447d87aac1036ad3f942ca4e2
Derek Shoemaker <shoemakerdr@gmail.com>
2017-01-23 22:44:54 +0000

    first try at arrayToList function

--- elqjs4.js
+++ elqjs4.js
@@ -145,4 +145,19 @@ the element at the given position in the list, or undefined when there is no
 such element.
 
 If you haven’t already, also write a recursive version of nth.
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
 */
+
+function arrayToList(array){
+  var i = 0; 
+  var list = {"value": array[i], "rest":arrayToList([array[i++]])};
+  return list;
+}
