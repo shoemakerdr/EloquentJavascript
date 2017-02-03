0baa37e654e41042c5320439267fe5330e72e26a
Derek Shoemaker <shoemakerdr@gmail.com>
2017-02-03 23:09:21 +0000

    added byName function to instructions

--- elqjs5.js
+++ elqjs5.js
@@ -68,6 +68,12 @@ can use the average function defined earlier in this chapter.
 Note that not all the mothers mentioned in the data are themselves present in 
 the array. The byName object, which makes it easy to find a person’s object from
 their name, might be useful here.
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
 */
 function ageDifference(object){
   var ages = 
