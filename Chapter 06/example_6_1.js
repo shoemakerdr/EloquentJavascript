993f1eb2084eb0d0699d1173efa731ba302ddf09
Derek Shoemaker <shoemakerdr@gmail.com>
2017-02-09 14:00:42 +0000

    finished example with right aligned numbers column

--- Chapter 06/example_6_1.js
+++ Chapter 06/example_6_1.js
@@ -151,6 +151,7 @@ UnderlinedCell.prototype.draw = function(width, height) {
     .concat([repeat("-", width)]);
 };
 
+/*
 // Takes data set as argument and converts it to cell format
 function dataTable(data) {
   var keys = Object.keys(data[0]);
@@ -160,6 +161,40 @@ function dataTable(data) {
   var body = data.map(function(row) {
     return keys.map(function(name) {
       return new TextCell(String(row[name]));
+    });
+  });
+  return [headers].concat(body);
+}
+*/
+
+// creates right-aligned text cell for numbers columns
+function RTextCell(text) {
+  TextCell.call(this, text);
+}
+RTextCell.prototype = Object.create(TextCell.prototype);
+RTextCell.prototype.draw = function(width, height) {
+  var result = [];
+  for (var i = 0; i < height; i++) {
+    var line = this.text[i] || "";
+    result.push(repeat(" ", width - line.length) + line);
+  }
+  return result;
+};
+
+// takes data set as argument and draws table with numbers column right aligned
+function dataTable(data) {
+  var keys = Object.keys(data[0]);
+  var headers = keys.map(function(name) {
+    return new UnderlinedCell(new TextCell(name));
+  });
+  var body = data.map(function(row) {
+    return keys.map(function(name) {
+      var value = row[name];
+      // This was changed:
+      if (typeof value == "number")
+        return new RTextCell(String(value));
+      else
+        return new TextCell(String(value));
     });
   });
   return [headers].concat(body);
