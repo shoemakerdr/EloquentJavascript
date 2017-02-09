a13dbd21ce64d3c3015459238c17e229cd87fa76
Derek Shoemaker <shoemakerdr@gmail.com>
2017-02-09 16:44:53 +0000

    finished first exercise in chapter 6-- a vector type

--- Chapter 06/exercise_6_1.js
+++ Chapter 06/exercise_6_1.js
@@ -14,3 +14,26 @@ vectors’ (the one in this and the parameter) x and y values.
 Add a getter property length to the prototype that computes the length of the
 vector—that is, the distance of the point (x, y) from the origin (0, 0).
 */
+function Vector(x, y){
+  this.x = x;
+  this.y = y;
+}
+
+Vector.prototype.plus = function(other){
+  return new Vector(this.x + other.x, this.y + other.y);
+}
+
+Vector.prototype.minus = function(other){
+  return new Vector(this.x - other.x, this.y - other.y);
+}
+
+Object.defineProperty(Vector.prototype, "length", {
+  get: function() {return Math.sqrt((this.x*this.x)+(this.y*this.y))}
+});
+
+
+// Test cases
+console.log(new Vector(1, 2));
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+console.log(new Vector(3, 4).length);
