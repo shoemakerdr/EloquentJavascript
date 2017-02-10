a1b580e8384f28efe7894104fa03be4844c1090d
Derek Shoemaker <shoemakerdr@gmail.com>
2017-02-10 15:34:10 +0000

    finished exercise 3 of chapter six. had to look up solution to understand problem correctly

--- Chapter 06/exercise_6_3.js
+++ Chapter 06/exercise_6_3.js
@@ -18,3 +18,61 @@ over the array using the interface you designed. Implement another object type
 RangeSeq that iterates over a range of integers (taking from and to arguments to
 its constructor) instead.
 */
+
+// I have to admit: this one totally stumped me. Had to look at solutions to 
+// understand it. Trying not to feel like an idiot for not understanding this.
+// Will comment a full explanation of the code.
+
+// Takes a sequence object as an argument, log current state of sequence, checks
+// boolean value of function next()
+function logFive(sequence) {
+  for (var i = 0; i < 5; i++){
+    if (!sequence.next())
+      break;
+    console.log(sequence.current());
+  }
+}
+
+// Constructor for a sequence of arrays
+function ArraySeq(array) {
+  this.position = -1;
+  this.array = array;
+}
+
+// Checks if position in index is greater than length of array. Starts at -1 in
+// case array is empty.
+ArraySeq.prototype.next = function() {
+  if (this.position >= this.array.length - 1) 
+    return false;
+  this.position++;
+  return true;
+};
+
+// Returns current element in array
+ArraySeq.prototype.current = function() {
+  return this.array[this.position];
+};
+
+// Constructor that creates sequence object for a range of numbers. Gives a now
+// state (which is set at one less than start)
+function RangeSeq(start, end) {
+  this.now = start - 1;
+  this.end = end;
+}
+
+// Returns false if now state is at end state. Else adds 1 to now state then 
+// returns true.
+RangeSeq.prototype.next = function() {
+  if (this.now >= this.end)
+    return false;
+  this.now++;
+  return true;
+};
+
+// Returns current now state
+RangeSeq.prototype.current = function() {
+  return this.now;
+};
+
+logFive(new ArraySeq([1, 2]));
+logFive(new RangeSeq(100, 1000));
