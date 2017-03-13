322f10b3eafe8464be89341b6c649c27f1d1dadb
Derek Shoemaker <shoemakerdr@gmail.com>
2017-03-13 13:52:30 +0000

    exercise 7.1

--- Chapter 07/exercise_7_1.js
+++ Chapter 07/exercise_7_1.js
@@ -27,59 +27,21 @@ is blank, it returns the "reproduce" object. If not, it searches for plants, and
 if it finds a plant, it returns the "eat" object. Else, if there is an empty
 space, it returns the "move" object.
 */
-PlantEater.prototype.act = function(view) {
-  var space = view.find(" ");
-  if (this.energy > 60 && space)
-    return {type: "reproduce", direction: space};
-  var plant = view.find("*");
-  if (plant)
-    return {type: "eat", direction: plant};
-  if (space)
-    return {type: "move", direction: space};
-};
-
-// dirPlus function makes direction changes calculable by adding the direction
-// change and the number of elements in the directions array to the index number
-// and then getting the modulus value. Positive n goes clockwise, negative n
-// goes counter-clockwise.
-function dirPlus(dir, n) {
-  var index = directionNames.indexOf(dir);
-  return directionNames[(index + n + 8) % 8];
-}
-// WallFollower object's default starting direction is south.
-function WallFollower() {
-  this.dir = "s";
-}
-/*
-The act method of this critter first assigns the WallFollower's default
-direction to the start variable. It uses the look method on the View object
-to check if the vector to just behind the critter's left (3 vectors
-counter-clockwise from the starting position) is open. If not, both the start
-value and default direction of the object are changed to the critter's left
-(2 vectors counter-clockwise from the original default direction). This is so
-the critter is always checking its left side first to make sure it is always
-following along the wall.
-*/
-WallFollower.prototype.act = function(view) {
-  var start = this.dir;
-  if (view.look(dirPlus(this.dir, -3)) != " ")
-    start = this.dir = dirPlus(this.dir, -2);
-  while (view.look(this.dir) != " ") {
-    this.dir = dirPlus(this.dir, 1);
-    if (this.dir == start) break;
-  }
-  return {type: "move", direction: this.dir};
-};
-
-
-
 
 // my code
 function SmartPlantEater() {
-    this.energy = 20;
-    this.dir = "someDirection";
+  this.energy = 30;
+  this.dir = "e";
 }
 
 SmartPlantEater.prototype.act = function() {
-    
-}
+  var space = view.find(" ");
+  if (this.energy > 90 && space)
+    return {type: "reproduce", direction: space};
+  var plants = view.findAll("*");
+  if (plants.length > 1)
+    return {type: "eat", direction: randomElement(plants)};
+  if (view.look(this.direction) != " " && space)
+    this.direction = space;
+  return {type: "move", direction: this.direction};
+};
