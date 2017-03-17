9ff50062e93c489b984836c8e3398a537c2c89fb
Derek Shoemaker <shoemakerdr@gmail.com>
2017-03-17 20:14:37 +0000

    completed chapter 9 exercises

---
+++ Chapter 09/exercise_9_2.js
@@ -0,0 +1,16 @@
+// Chapter 9: Regular Expressions
+
+/*
+Exercise 2: Quoting style
+
+Imagine you have written a story and used single quotation marks throughout to
+mark pieces of dialogue. Now you want to replace all the dialogue quotes with
+double quotes, while keeping the single quotes used in contractions like aren’t.
+
+Think of a pattern that distinguishes these two kinds of quote usage and craft a
+call to the replace method that does the proper replacement.
+*/
+
+var text = "'I'm the cook,' he said, 'it's my job.'";
+
+console.log(text.replace(/\B'/g, "\""));
